cc3888d3cb77e6a801915ab0d0d6bc65c5275f7e
UH4CK3R <skfkdmldlsrk@naver.com>
2016-12-19 22:33:42 +0900

    .

--- html/include/dbconn.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "config.php";
-
-$link = mysqli_connect($ip, $id, $pw, $db);
-
-mysqli_query($link,"set names utf8");
-
-
-?>
--- html/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-
-echo "<script>alert('Logout.');location.href='./';</script>";
-
-?>
